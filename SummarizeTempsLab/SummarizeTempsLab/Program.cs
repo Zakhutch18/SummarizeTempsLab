a65f7671fa9cb159330f4ec4fb42c5139b939529
Zakhutch18 <59981862+Zakhutch18@users.noreply.github.com>
2020-02-15 09:44:24 -0600

    Update Program.cs

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -8,6 +8,64 @@ namespace SummarizeTempsLab
         static void Main(string[] args)
         {
             // temperature data is in temps.txt
+           // write out promt to the console
+            string filename;
+            Console.WriteLine("Enter a filename");
+            filename = Console.ReadLine();
+
+
+            if(File.Exists(filename))
+            {
+
+                Console.WriteLine("File Exists");
+                Console.WriteLine("Enter Threshold");
+                string input;
+                int threshold;
+                input = Console.ReadLine();
+                threshold = int.Parse(input);
+                int sumTemps = 0;
+                int numAbove = 0;
+                int numBelow = 0;
+                int tempCount = 0;
+
+                using (StreamReader sr = File.OpenText(filename))
+                {
+                    string line = sr.ReadLine();
+                    int temp;
+                    
+                    while (line != null)
+                    {
+                        temp = int.Parse(line);
+                        sumTemps += temp;
+                        tempCount += 1;
+                        
+                        if(temp>= threshold)
+                        {
+                            numAbove += 1;
+
+                        }
+                        else
+                        {
+                            numBelow += 1;
+                        }
+                        line = sr.ReadLine();
+                    }
+
+                }
+
+                Console.WriteLine("Temperatures above = " + numAbove);
+                Console.WriteLine("Temperatures below = " + numBelow);
+               
+                    int average = sumTemps / tempCount;
+
+                Console.WriteLine("Average temp = " + average);
+
+            }
+            else
+	        {
+                Console.WriteLine("File Does Not Exist");
+
+	        }
         }
     }
 }
